93b3b55e69e59e9ff3ed343ea9aa339561476acb
clarasantos15 <150clara.santos@gmail.com>
2026-08-19 18:00:18 -0300

    CODIGO

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
 alert("Olá Mundo!");
+
+var nasc = 2009;
+let nome = "Clara";
+const viva = true;
+let altura = 1.64;
+
+if(viva){
+    let saudacao = "Olá" + nome + "!";
+    let msg = `Altura ${altura}m | Idade ${2026 - nasc}`;
+    alert(saudacao + "\n"+msg);
+
+}else{
+    alert("R.I.P");
+}
+
+
+
+
+
